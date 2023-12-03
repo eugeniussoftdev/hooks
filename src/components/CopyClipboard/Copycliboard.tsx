cdae6ca97a932c0e8914d1478c468ab586001b3c
Eugene <eugeniussoftdev@gmail.com>
2023-12-03 21:16:40 +0200

    add useCopytoClipboard hook

--- src/components/CopyClipboard/Copycliboard.tsx
+++ src/components/CopyClipboard/Copycliboard.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useCopyToClipboard } from "../../hooks/useCopyToClipboard";
 
 export const Copycliboard = () => {
+  const [text, setText] = useState("");
+  const { copyToClipboard } = useCopyToClipboard();
+
+  const copyToClipboardHandler = () => {
+    console.log(text);
+    copyToClipboard(text);
+  };
+
+  const pasteText = async () => {
+    try {
+      const text = await navigator.clipboard.readText();
+      console.log("Pasted content: ", text);
+    } catch (err) {
+      console.error("Failed to read clipboard contents: ", err);
+    }
+  };
+
   return (
     <div>
       <div>
-        <input type="text" placeholder="text" />
-        <button type="button">Copy</button>
+        <input
+          type="text"
+          placeholder="text"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="button" onClick={copyToClipboardHandler}>
+          Copy
+        </button>
       </div>
       <div>
         <input type="text" />
-        <button type="button">Paste</button>
+        <button type="button" onClick={pasteText}>
+          Paste
+        </button>
       </div>
     </div>
   );
